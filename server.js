281da7c751e4e09728140eabe8d76ad6e0709239
mjg1123 <53385037+mjg1123@users.noreply.github.com>
2019-12-16 15:54:19 -0500

    port updated in json file

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.static(__dirname + '/dist/'));
 app.get(/.*/, function (req, res) {
   res.sendFile(__dirname + '/dist/index.html');
 })
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 app.listen(port);
 
 console.log("server started");
